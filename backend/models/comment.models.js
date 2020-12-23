7c16f3c6e49428d812d46a8278d3b77bfc29200e
Sodan Labs <sodan.labs@gmail.com>
2020-12-23 15:30:01 +0100

    mod[feat]: Change type of comment text STRING to TEXT

--- backend/models/comment.models.js
+++ backend/models/comment.models.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, Sequelize) => {
         },
         text: {
             allowNull: false,
-            type     : Sequelize.STRING
+            type     : Sequelize.TEXT
         }
     });
 
